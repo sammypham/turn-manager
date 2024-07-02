049bad6e98cc60939eea2f761358306ad8badd98
sammypham <sammyphams@gmail.com>
2024-07-01 22:28:55 -0700

    initial commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,15 @@ import { Outlet } from 'react-router-dom'
 
 import "./Header.css"
 
+const dayjs = require('dayjs')
+
 const Header = () => {
     return (
         <>
             <div className='site-header'>
-
+                <div style={{ height: "100%", alignContent: "center", padding: 10 }}>
+                    {dayjs().format("ddd, MMMM D YYYY, hh:mm A")}
+                </div>
             </div>
             <Outlet />
         </>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import Header from "../../components/Header/Header";
 import "./Home.css"
 
 import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
 
 const technicians = [
     {
@@ -66,26 +68,133 @@ const technicians = [
     },
 ];
 
-
 const Home = () => {
+    const [signInModalOpen, setSignInModalOpen] = useState(false);
+    const [serviceMenuModalOpen, setServiceMenuModalOpen] = useState(false);
+
+    const [nextTechnician, setNextTechnician] = useState(null);
+
+    const openSignInModal = () => {
+        setSignInModalOpen(true);
+    }
+
+    const closeSignInModal = () => {
+        setSignInModalOpen(false);
+    }
+
+    const openServiceMenuModal = () => {
+        setServiceMenuModalOpen(true);
+    }
+
+    const closeServiceMenuModal = () => {
+        setServiceMenuModalOpen(false);
+    }
+
+    const clickClearTracker = () => {
+        const confirm = window.confirm("Are you sure you want to clear the turn tracker?");
+
+        if (confirm) {
+
+        }
+    }
+
+    const createTechnicianCard = (technician, index) => {
+        return (
+            <button className="modal-card" key={index}>
+                {technician ? technician.name : "Technician Name"}
+            </button>
+        )
+    }
+
+    const createServiceCard = (service, index) => {
+        return (
+            <button className="modal-card" key={index}>
+                {service ? service.name : "Service Name"}
+            </button>
+        )
+    }
+
     return (
         <>
             <Header />
+            {
+                signInModalOpen
+                &&
+                <div
+                    className="modal-background"
+                    onClick={(e) => { if (e.currentTarget === e.target) { closeSignInModal() } }}
+                >
+                    <div className="modal-container technicians-modal-container">
+                        <div className="modal-header">
+                            Technicians
+                        </div>
+                        <button onClick={closeSignInModal} className="close-button">
+                            <CloseIcon />
+                        </button>
+                        <div className="modal-content-wrapper">
+                            <div className="modal-content">
+                                {technicians
+                                    .sort((a, b) => a.name.localeCompare(b.name))
+                                    .map((technician, index) =>
+                                        createTechnicianCard(technician, index)
+                                    )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+            {
+                serviceMenuModalOpen
+                &&
+                <div
+                    className="modal-background"
+                    onClick={(e) => { if (e.currentTarget === e.target) { closeServiceMenuModal() } }}
+                >
+                    <div className="modal-container services-modal-container">
+                        <div className="modal-header">
+                            Services
+                        </div>
+                        <button onClick={closeServiceMenuModal} className="close-button">
+                            <CloseIcon />
+                        </button>
+                        <div className="modal-content-wrapper">
+                            <div className="modal-content">
+                                {createServiceCard()}
+                                {createServiceCard()}
+                                {createServiceCard()}
+                                {createServiceCard()}
+                                {createServiceCard()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
             <div className="home__turn-tracker-container">
                 <div className="home__turn-tracker-header">
-                    Hi
+                    <button onClick={openSignInModal} className="header-button sign-in">
+                        Sign In
+                    </button>
+                    <div style={{ height: "100%", alignContent: "center" }}>
+                        Next Technician: <b>{nextTechnician ? nextTechnician.name : "none"}</b>
+                    </div>
+                    <button onClick={clickClearTracker} className="header-button clear">
+                        Clear
+                    </button>
                 </div>
                 <div className="home__turn-tracker-wrapper">
                     {technicians.map((technician, techIndex) =>
                         <div key={techIndex} className="home__turn-tracker-row">
                             <div className="technician_name">
-                                {technician.name}
+                                {technician.name} ({technician.turns.length})
                             </div>
                             {technician.turns.map((turn, turnIndex) =>
-                                <button key={`${techIndex} ${turnIndex}`} className="turn-box">
-                                    {turn.substring(0, 3)}
+                                <button onClick={openServiceMenuModal} key={`${techIndex} ${turnIndex}`} className="turn-box">
+                                    {turn.substring(0, 4)}
                                 </button>
                             )}
+                            <button onClick={openServiceMenuModal} className="turn-box add-turn">
+                                <AddIcon />
+                            </button>
                         </div>
                     )}
                 </div>
