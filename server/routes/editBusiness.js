e5c13c17505c15c208a703256ba8510e7c47e969
nianguye <nianguye@ucdavis.edu>
2024-07-09 15:25:39 -0700

    Delete all other documents

--- server/routes/editBusiness.js
+++ server/routes/editBusiness.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { Business} = require('../models/business');
+const { Technician} = require('../models/technician');
+const { Service} = require('../models/service');
+const { Sign_In} = require('../models/sign_in');
+const { Service_Record} = require('../models/service_record');
 
 router.post('/', async (req, res) => {
     try {
@@ -22,6 +26,15 @@ router.post('/', async (req, res) => {
 router.delete('/', async (req, res) => {
     try {
         await Business.findByIdAndDelete(req.session.currentBusiness._id)
+        await Service.deleteMany({business : req.session.currentBusiness._id})
+        await Sign_In.deleteMany({business : req.session.currentBusiness._id})
+
+        const techList = await Technician.find({business : req.session.currentBusiness._id})
+        console.log(techList)
+        techList.forEach(async(givenTech, index) => {
+            await Service_Record.deleteMany({technician: givenTech._id})
+        });
+        await Technician.deleteMany({business : req.session.currentBusiness._id})
         req.session.currentBusiness = undefined;
         res.status(201).json({})
     } catch (error) {
